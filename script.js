abc8494adfc63a7f797571ae4417fcf6b3825d82
Mau <maura2403@gmail.com>
2020-07-18 16:11:23 -0300

    sigo boludeando, hago un intento de login chotisimo pa practicar aunque va re mal

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 
-function saludo(){
-   let x = document.getElementById("nombre");
-   console.log(x.value)
-   alert("Hola " + x.value)
+let nombreUsuario = document.getElementById("usuario"),
+    usuario = nombreUsuario.value;
+let valorContrasena = document.getElementById("contra"),
+    contrasena = valorContrasena.value
+let correcto, correctisimo;
+function verificar(){
+    if (usuario.length >= 8){
+        correcto == true;
+        
+    }
+    if(contrasena.length >= 8){
+        correctisimo == true;
+    }
+}
+
+function botoncito(){
+    console.log(usuario.length)
+    verificar()
+    if(correcto== true && correctisimo == true){
+         console.log(usuario + " " + contrasena)
+    }else if(correcto != true || correctisimo != true){
+        alert("datos inválidos, tanto el usuario como la contraseña deben tener al menos 8 caracteres")
+    } 
+   
+
 }
